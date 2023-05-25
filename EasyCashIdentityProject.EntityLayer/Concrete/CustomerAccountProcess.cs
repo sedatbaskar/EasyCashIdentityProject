743f4a70a41b540f8d2d59f462a53b0dc0e40148
Sedat Baskar <sedatbaskar@gmail.com>
2023-05-25 08:32:18 +0300

    ders5..

--- EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccountProcess.cs
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccountProcess.cs
@@ -8,7 +8,7 @@ namespace EasyCashIdentityProject.EntityLayer.Concrete
 {
     public class CustomerAccountProcess
     {
-        public int CustomerAccountID { get; set; }
+        public int CustomerAccountProcessID { get; set; }
         public string ProcessType { get; set; }
         public decimal Amount { get; set; }
         public DateTime ProcessDate { get; set; }
